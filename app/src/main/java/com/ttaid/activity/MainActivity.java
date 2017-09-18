d67a6c2e74af92b7dba7f1792c3c215fafbec402
汪帆 <sail032@live.cn>
2017-09-18 15:39:46 +0800

    添加讯飞aiui

--- app/src/main/java/com/ttaid/activity/MainActivity.java
+++ app/src/main/java/com/ttaid/activity/MainActivity.java
@@ -637,6 +637,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onResume() {
+
 //        BroadcastManager.sendBroadcast(BroadcastManager.ACTION_VOICE_WAKE_CLOSE, null);
         BroadcastManager.sendBroadcast(BroadcastManager.ACTION_VOICE_EMULATE_KEY_CLOSE, null);
         mQueue = Volley.newRequestQueue(this);
